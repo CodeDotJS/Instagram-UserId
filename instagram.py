98c7de5c997d9189309884f135757cef72ccd0dc
Rishi Giri <rushforlinux@gmail.com>
2015-12-15 00:04:14 +0530

    Update instagram.py

--- instagram.py
+++ instagram.py
@@ -1,2 +1,25 @@
 import json
+
 import urllib2
+
+import sys
+
+user = sys.argv[1]
+
+def extract(user=user):
+
+    f = urllib2.urlopen('http://jelled.com/ajax/instagram?do=username&username='+user)
+
+    get_JSON = json.load(f)
+
+    files = open(str(user) + '.json', 'w')
+
+    files.write(str (get_JSON))
+
+    files.close()
+
+    return get_JSON["data"][0]["id"]
+
+if __name__ == "__main__":
+
+    print "The Id of " + user + ' is ' + extract()
